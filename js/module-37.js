a4504387c5eeb69a63c3f449c1f26be3de810396
Vadym Chutur <falloutgreck@gmail.com>
2024-03-15 03:15:15 +0200

    Update module-37.js

--- js/module-37.js
+++ js/module-37.js
@@ -6,19 +6,72 @@ const palylist = {
   author: 'Mango',
 };
 
-const {
-  raiting,
-  name,
-  tracks,
-  trackCount: numberOfTracks = 0,
-  author = 'user',
-} = palylist;
+// const {
+//   raiting,
+//   name,
+//   tracks,
+//   trackCount: numberOfTracks = 0,
+//   author = 'user',
+// } = palylist;
 
-console.log(raiting);
-console.log(name);
-console.log(tracks);
-console.log(numberOfTracks);
-console.log(author);
+// console.log(raiting);
+// console.log(name);
+// console.log(tracks);
+// console.log(numberOfTracks);
+// console.log(author);
+
+// const profile = {
+//   name: 'Jacques Gluke',
+//   tag: 'jgluke',
+//   location: 'Ocho Rios, Jamaica',
+//   avatar: 'https://s3.amazonaws.com/uifaces/faces/twitte/r_oy/128.jpg',
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+
+// const {
+//   name,
+//   tag,
+//   location,
+//   avatar,
+//   stats: { followers: myFollowers = 0 },
+// } = profile;
+
+// console.log(name, tag, location, avatar, myFollowers);
+
+const rgb = [255, 100, 80];
+
+const [red, green, blue] = rgb;
+
+console.log('red: ', red, 'green: ', green, 'blue: ', blue);
+
+const authors = {
+  kiwi: 4,
+  poly: 7,
+  ajax: 9,
+  mango: 6,
+};
+
+// const keys = Object.keys(authors);
+
+// for (const key of keys) {
+//   console.log(key);
+//   console.log(authors[key]);
+// }
+
+// console.log(Math.max(...raitings));
+
+const entries = Object.entries(authors);
+
+console.log(entries);
+
+for (const entriy of entries) {
+  const [name, raiting] = entriy;
+  console.log(name, raiting);
+}
 
 const profile = {
   name: 'Jacques Gluke',
@@ -31,3 +84,7 @@ const profile = {
     likes: 1308,
   },
 };
+
+const { name, tag, location, ...restProps } = profile;
+
+console.log(restProps);
