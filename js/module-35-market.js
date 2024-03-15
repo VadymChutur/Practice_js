7d101d51f2669332ffb78727a450c1ca65576d50
Vadym Chutur <falloutgreck@gmail.com>
2024-03-15 16:45:15 +0200

    upd

--- js/module-35-market.js
+++ js/module-35-market.js
@@ -13,7 +13,22 @@ const cart = {
     return this.items;
   },
   add(product) {
-    this.items.push(product);
+    const { items } = this;
+
+    for (const item of items) {
+      if (item.name === product.name) {
+        console.log('Цей продукт вже є');
+        item.quantity += 1;
+        return;
+      }
+    }
+
+    const newProdact = {
+      ...product,
+      quantity: 1,
+    };
+
+    items.push(newProdact);
     // for (const item of this.items) {
     //   item.quantity = 1;
     //   if (item.quantity) {
@@ -21,9 +36,42 @@ const cart = {
     //   }
     // }
   },
-  remove(product) {},
-  clear() {},
-  countTotalPrice() {},
+  remove(product) {
+    const { items } = this;
+
+    for (let i = 0; i < items.length; i += 1) {
+      const { name } = items[i];
+
+      if (product === name) {
+        // console.log(items[i]);
+        // console.log(i);
+        // console.log(name);
+        return items.splice(i, 1);
+      }
+    }
+
+    // for (const item of this.items) {
+    //   if (product === item.name) {
+    //     console.log(item);
+    //     this.items.splice()
+    //   }
+    // }
+  },
+  clear() {
+    this.items = [];
+  },
+  countTotalPrice() {
+    const { items } = this;
+    let total = 0;
+
+    for (const { price, quantity } of items) {
+      // console.log(price);
+      total += price * quantity;
+    }
+
+    return total;
+  },
+
   increaseQuantity(productName) {},
   decreaseQuantity(productName) {},
 };
@@ -34,5 +82,22 @@ cart.add({ name: 'apple', price: 50 });
 cart.add({ name: 'lemon', price: 60 });
 cart.add({ name: 'lemon', price: 60 });
 cart.add({ name: 'strawberry', price: 110 });
+cart.add({ name: 'strawberry', price: 110 });
+cart.add({ name: 'strawberry', price: 110 });
+
+console.table(cart.getItems());
+
+cart.remove('apple');
+
+console.table(cart.getItems());
+
+cart.add({ name: 'apple', price: 50 });
+cart.add({ name: 'apple', price: 50 });
+
+console.table(cart.getItems());
+
+// cart.clear();
+
+console.log(cart.countTotalPrice());
 
 console.table(cart.getItems());
--- js/module-37.js
+++ js/module-37.js
@@ -77,7 +77,7 @@ const profile = {
   name: 'Jacques Gluke',
   tag: 'jgluke',
   location: 'Ocho Rios, Jamaica',
-  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitte/r_oy/128.jpg',
+  //   avatar: 'https://s3.amazonaws.com/uifaces/faces/twitte/r_oy/128.jpg',
   stats: {
     followers: 5603,
     views: 4827,
@@ -88,3 +88,36 @@ const profile = {
 const { name, tag, location, ...restProps } = profile;
 
 console.log(restProps);
+
+const showProfileInfo = function ({ name, tag, location, avatar, stats }) {
+  //   const { name, tag, location, avatar, stats } = userProfile;
+  //   console.log(userProfile);
+  console.log(name, tag, location, avatar, stats);
+};
+
+showProfileInfo(profile);
+
+const makeProfileMarkup = function ({
+  avatar = 'https://i.pravatar.cc/400?img=6',
+  name,
+  tag,
+  location = 'PlanetEarth',
+  stats: { followers, views, likes },
+}) {
+  return `<div>
+    <img src="${avatar}" alt="user avatar">
+    <p>${name}<span>@${tag}</span></p>
+    <p>Location: ${location}</p>
+        <ul>
+            <li>Followers: ${followers}</li>
+            <li>Views: ${views}</li>
+            <li>Likes: ${likes}</li>
+        </ul>
+    </div>`;
+};
+
+const markup = makeProfileMarkup(profile);
+
+console.log(markup);
+
+document.body.insertAdjacentHTML('afterbegin', markup);
