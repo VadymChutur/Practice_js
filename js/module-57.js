cd84f0b4a838d31a507c01c2f73b34bb8f9dc6fa
Vadym Chutur <falloutgreck@gmail.com>
2024-04-22 14:16:09 +0300

    Update module-57.js

--- js/module-57.js
+++ js/module-57.js
@@ -37,29 +37,40 @@ class Hero {
 // const mango = new Hero({ name: 'Mango', xp: 1000 });
 
 class Warrior extends Hero {
-  constructor({ name, xp, weapon = 'Sword' } = {}) {
-    super({ name, xp });
-    console.log(name, xp, weapon);
+  constructor({ weapon = 'Sword', ...restProps } = {}) {
+    super(restProps);
+    console.log(restProps);
 
     this._weapon = weapon;
+  }
+
+  attack() {
+    console.log(`${this._name} атакує зброєю ${this._weapon}`);
   }
 }
 
 class BatleMage extends Warrior {
-  constructor({ name, spelss }) {
-    super({ name });
+  constructor({ spells = 'Wind Blade', ...restProps } = {}) {
+    super(restProps);
 
-    this._spels = spelss;
+    this._spells = spells;
+  }
+
+  cast() {
+    console.log(`${this._name} кастує ${this._spells}`);
   }
 }
 
 const mango = new Warrior({ name: 'Mango', xp: 1000, weapon: 'Axe' });
-const kiwi = new BatleMage({ name: 'Kiwi', spelss: 'fireball' });
+const kiwi = new BatleMage({ name: 'Kiwi', spells: 'Fireball' });
 
 console.log(mango);
+mango.attack();
 mango.gainXp(1000);
 console.log(mango);
 
 console.log(kiwi);
+kiwi.attack();
+kiwi.cast();
 kiwi.gainXp(3000);
 console.log(kiwi);
