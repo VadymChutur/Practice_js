72bc87b3244592c4b5502e5edd8c2a3ed790c1d9
Vadym Chutur <falloutgreck@gmail.com>
2024-05-03 16:20:43 +0300

    upd

---
+++ js/products.js
@@ -0,0 +1,26 @@
+export default [
+{
+  name: 'Сервоприводи',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, veritatis explicabo quas nihil corporis quaerat accusamus quo vel laboriosam tempore quam minus eaque amet neque quae assumenda ut recusandae itaque.',
+  price: 2000,
+  available: true,
+  onSale: true,
+},
+{
+  name: 'Сервоприводи',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, veritatis explicabo quas nihil corporis quaerat accusamus quo vel laboriosam tempore quam minus eaque amet neque quae assumenda ut recusandae itaque.',
+  price: 2000,
+  available: true,
+  onSale: true,
+},
+{
+  name: 'Сервоприводи',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, veritatis explicabo quas nihil corporis quaerat accusamus quo vel laboriosam tempore quam minus eaque amet neque quae assumenda ut recusandae itaque.',
+  price: 2000,
+  available: true,
+  onSale: true,
+};
+]
