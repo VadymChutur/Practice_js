60c2817674fc2ac5e3570d6770dea7408e20e7f8
Vadym Chutur <falloutgreck@gmail.com>
2024-05-15 12:47:28 +0300

    upd

---
+++ js/module-70.js
@@ -0,0 +1,18 @@
+const refs = {
+  output: document.querySelector('.js-output'),
+  clearBtn: document.querySelector('.js-clear'),
+};
+
+window.addEventListener('keypress', onKeypress);
+refs.clearBtn.addEventListener('click', onClearOutput);
+
+function onKeypress(event) {
+  console.log(event);
+  console.log('event.key: ', event.key);
+
+  refs.output.textContent += event.key;
+}
+
+function onClearOutput() {
+  refs.output.textContent = '';
+}
